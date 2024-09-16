79fef454accb56e3214e3354f9355d199e8e6026
hdlguy <hdlguy@hdlguy.com>
2024-09-16 12:58:44 -0400

    trying to work with Nano 33 IoT

---
+++ mini_keyboard/arduino/blink/blink.ino
@@ -0,0 +1,14 @@
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(198);
+}
