f4a10700ccdeae513d59bcdce9bb19671721c26c
hdlguy <hdlguy@hdlguy.com>
2024-09-26 10:39:28 -0400

    added serial console

--- mini_keyboard/arduino/blink/blink.ino
+++ mini_keyboard/arduino/blink/blink.ino
@@ -2,14 +2,23 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+    // console
+    Serial.begin(115200);
 }
 
 // the loop function runs over and over again forever
+uint32_t whilecount=0;
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(50); 
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(450);
+    //
+    Serial.print(whilecount, HEX);
+    Serial.println(": Hello World!");
+    //
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(50); 
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(450);
+    //
+    whilecount++;
 }
